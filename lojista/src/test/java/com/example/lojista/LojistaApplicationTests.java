4a36d2c34e33fe1d2a3dd618961f43b3368534a0
CaioRangel1 <142802207+CaioRangel1@users.noreply.github.com>
2024-04-18 08:26:54 -0300

    Add files via upload

---
+++ lojista/src/test/java/com/example/lojista/LojistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lojista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
